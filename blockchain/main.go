22114c5f69a4f100ac85310cb71fa2cbd478d365
xoreo <mattnappo@gmail.com>
2018-12-31 16:00:52 -0500

    fixed main.go

--- blockchain/main.go
+++ blockchain/main.go
@@ -8,21 +8,22 @@ import (
 )
 
 var (
-	populateFlag = flag.Int("populate", 10, "Populate a blockchain with x blocks")
+	populateFlag = flag.Int("populate", 0, "Populate a blockchain with x blocks")
 	loadFlag     = flag.String("load", "", "Load and validate a blockchain from memory")
 )
 
 func main() {
 	flag.Parse()
 	if *populateFlag > 0 {
+		fmt.Println("POPULATE FLAG")
 		chain, err := blockchain.PopulateChain(*populateFlag)
 		if err != nil {
 			panic(err)
 		}
 		chain.WriteBlockchainToMemory()
 	}
-
 	if *loadFlag != "" {
+		fmt.Println("LOAD FLAG")
 		// name := fmt.Sprintf("data/chains/chain_%s.json", *loadFlag)
 		chain, err := blockchain.ReadBlockchainFromMemory(*loadFlag)
 		if err != nil {
