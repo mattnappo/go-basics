aa229aa727e89fc3995d8416db9284ece294ac57
xoreo <mattnappo@gmail.com>
2019-02-05 07:58:42 -0500

    Buf size bugs

--- blockchain/networking/handler.go
+++ blockchain/networking/handler.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net"
 
-	"github.com/xoreo/go-basics/blockchain/types/block"
 	"github.com/xoreo/go-basics/blockchain/types/blockchain"
 	"github.com/xoreo/go-basics/blockchain/types/transaction"
 )
@@ -13,23 +12,24 @@ import (
 // HandleConnection - Handle a connection with a client
 func HandleConnection(conn net.Conn, channel chan *blockchain.Blockchain) error {
 	defer conn.Close()
-	cChain := <-channel
+	// cChain := <-channel
 	// fmt.Println(cChain.String())
 	// Wait for the client to send their transactions
 
 	// readBuffer := []byte{} // This doesn't really work
-	readBuffer := make([]byte, 1000000)                     // 1 KiB
-	txnsBuffer := make([]*transaction.Transaction, 1000000) // Transaction buffer for deserialization
+	readBuffer := make([]byte, 233) // 1 KiB
+	// txnsBuffer := make([]*transaction.Transaction, 1000000) // Transaction buffer for deserialization
 
-	buffer := &transaction.Transaction{}
+	// buffer := &transaction.Transaction{}
 
 	for {
-		size, err := conn.Read(readBuffer) // Read into buffer
+		bufSize, err := conn.Read(readBuffer) // Read into buffer
 		// fmt.Println(string(buffer))
-		if err != nil || size <= 0 {
+		if err != nil || bufSize <= 0 {
 			return err
 		}
-		fmt.Printf("read buffer: %sdone\n", string(readBuffer))
+		fmt.Printf("size: %d\n", bufSize)
+		fmt.Printf("read buffer: %s\n", string(readBuffer))
 
 		// Unmarshal the data
 		// noNewlineReadBuffer := string(readBuffer)
@@ -41,8 +41,12 @@ func HandleConnection(conn net.Conn, channel chan *blockchain.Blockchain) error 
 		// 	fmt.Println(err)
 		// 	return err
 		// }
-		err = json.Unmarshal(readBuffer, buffer)
+		txnsBuffer := make([]*transaction.Transaction, bufSize) // Transaction buffer for deserialization
+		fmt.Println("       before unmarshal")
+		err = json.Unmarshal(readBuffer, &txnsBuffer)
+		fmt.Println("working after unmarshal")
 		if err != nil {
+			fmt.Println(err)
 			return err
 		}
 		fmt.Println("txn buffer: ")
@@ -50,17 +54,17 @@ func HandleConnection(conn net.Conn, channel chan *blockchain.Blockchain) error 
 		break
 	}
 
-	newBlock, err := block.NewBlock(
-		cChain.Blocks[len(cChain.Blocks)-1].Index,
-		txnsBuffer,
-		cChain.Blocks[len(cChain.Blocks)-1].Hash,
-	)
+	// newBlock, err := block.NewBlock(
+	// 	cChain.Blocks[len(cChain.Blocks)-1].Index,
+	// 	txnsBuffer,
+	// 	cChain.Blocks[len(cChain.Blocks)-1].Hash,
+	// )
 
-	if err != nil {
-		return err
-	}
-	fmt.Println("after creating new block: sjowing new block nwo:")
-	fmt.Println(newBlock.String())
+	// if err != nil {
+	// 	return err
+	// }
+	// fmt.Println("after creating new block: sjowing new block nwo:")
+	// fmt.Println(newBlock.String())
 	return nil
 
 	// err = blockchain.Validate(newBlock, cChain.Blocks[len(cChain.Blocks)-1])
